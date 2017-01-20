700f508d44ede0ac169de05c9bb99459e0c22046
綾村豊 <yutaka.ayamura@beeskynet.com>
2017-01-20 14:19:17 +0900

    diff_logs_time.py追加。ログ間時間差の算出、集計。

---
+++ diff_logs_time.py
@@ -0,0 +1,66 @@
+'''
+ユーザ別にログ間の時間差を算出し、
+時間差ごとのログ件数を集計する
+'''
+import pandas as pd
+from datetime import datetime
+import numpy
+import math
+
+# ログを読み込み
+logs = pd.read_csv('log/log.log', index_col=None)
+
+# 日付、時間文字列より日時値を作成
+logs['dtm'] = logs[['date', 'time']].apply(lambda x: datetime.strptime(x[0] + ' ' + x[1], "%Y-%m-%d %H:%M:%S"), axis=1)
+df = logs.drop(['date', 'time'], axis=1)
+
+# ユーザ、日付でソート
+df = df.sort_values(by=['user', 'dtm']).reset_index()
+
+# 1行ずらしてユーザ、日時を追加結合
+df2 = df.drop(0)
+df2 = df2.append({'user': '', 'dtm': None}, ignore_index=True)
+df['user2'] = df2['user']
+df['dtm2'] = df2['dtm']
+
+# 時間差を算出
+def time_diff(x):
+	if x[0] == x[1]:
+		# ユーザが同一の場合のみ差異を算出
+		return x[3] - x[2]
+	return None
+df['diff'] = df[['user', 'user2', 'dtm', 'dtm2']].apply(time_diff, axis=1)
+
+# 時間差をフォーマット
+def format_diff(x):
+	sec0 = x / numpy.timedelta64(1, 's')
+	(minu, sec) = divmod(sec0, 60)
+	(hour, minu) = divmod(minu, 60)
+	if sec0 < 10:
+		return '%02d:%02d:%02d秒' % (hour, minu, sec)
+	elif 10 <= sec0 < 30:
+		return '00:00:10~30秒'
+	elif 30 <= sec0 < 60:
+		return '00:00:30~60秒'
+	elif 60 <= sec0 < 300:
+		return '00:01:00~5分'
+	elif 300 <= sec0 < 1800:
+		return '00:05:00~30分'
+	elif 1800 <= sec0 < 3600:
+		return '00:30:00~1時間'
+	elif 3600 <= sec0 < 18000:
+		return '01:00:00~5時間'
+	elif 18000 <= sec0:
+		return '05:00:00~'
+	elif math.isnan(sec0):
+		return 'NaN'
+	return 'err'
+df['fdiff'] = df['diff'].apply(format_diff)
+
+# ユーザ数=時間差NaNであることを確認
+# print(len(df.drop_duplicates('user')))
+# print(len(df[df['fdiff'] == 'NaN']))
+
+# 集計、CSV出力
+df[df['fdiff'] != 'NaN'].groupby('fdiff').size().reset_index(name='count').to_csv('output/diff_logs_time.csv')
+
